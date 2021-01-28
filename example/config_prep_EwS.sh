1c6cfc45b60c920b9594f3cb20e2f85541a4e5ec
Gautier Maud <maud.j.gautier@gmail.com>
2021-01-28 11:05:08 +0100

    Add create_per_base_coverage_table

--- example/config_prep_EwS.sh
+++ example/config_prep_EwS.sh
@@ -18,14 +18,31 @@
 output_folder=/data/kdi_prod/project_result/726/27.01/results/tests/
 if [ ! -d $output_folder ] ; then mkdir $output_folder ; fi
 input_folder=/data/kdi_prod/project_result/726/27.01/results/1_Data/
+gtf_name=201201_Ewing_neos_NEW_IDs
 
 # Step 1: Prepare BED file of regions to cover for each neotranscript
 sh ./src/sh/gtf_to_bed.sh \
-	-i $input_folder/201201_Ewing_neos_NEW_IDs.gtf \
-	-o $output_folder/genomic_regions.bed \
+	-i ${input_folder}/${gtf_name}.gtf \
+	-o ${output_folder}/genomic_regions.bed \
 	-e 50000 \
 	-u "gene"
 
+
+# Sort chrom sizes in correct order
+chrom_sizes_hg19="/data/annotations/pipelines/Human/hg19/genome/chrom_hg19.sizes"
+chrom_sizes_sorted=${input_folder}/chrom_hg19.sizes.sorted
+grep "chr[123456789]" ${chrom_sizes_hg19} | grep -v random | grep -v hap > ${chrom_sizes_sorted}
+grep "chrM" ${chrom_sizes_hg19} >> ${chrom_sizes_sorted}
+grep "chr[XY]" ${chrom_sizes_hg19} >> ${chrom_sizes_sorted}
+
+
 # Step 2: Prepare per-base coverage files
+name=EW_merged
+sh ./src/sh/create_per_base_coverage_table.sh \
+	-i ${input_folder}/${name}.bam \
+	--bed ${output_folder}/genomic_regions.bed \
+	--gs ${input_folder}/chrom_sizes_in_sorted_order.sizes \
+	-o ${output_folder}/${name}.against.${gtf_name}
+
 
 
---
+++ src/sh/create_per_base_coverage_table.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env sh
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                                                                              #
+# create_per_base_coverage_table.sh                                            #
+#                                                                              #
+# By: Maud Gautier <https://github.com/MaudGautier>, 2021                      #
+#                                                                              #
+# Broad permissions are granted to use, modify, and distribute this software   #
+# as specified in the MIT License included in this distribution's LICENSE file.#
+#                                                                              #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Creates per-base coverage file (`-o`) on regions defined by input BED file 
+# (`--bed|-b`) from input file that can be in either a BED or a BAM format
+# (`-i`). The process is sped up if a chrom_sizes file is provided (`--gs|-g`).
+# If the input file is in a BIGWIG format instead of BED or BAM, the option 
+# `--bw|--bigwig` should be turned on. 
+
+
+## Requirements:
+# - GNU awk
+# - GNU sort
+# - bedtools
+# - bigWigToBedGraph
+
+
+## Input
+# The input chrom_sizes should be in this form:
+# chr1  249250621
+# chr2  243199373
+# chr3  198022430
+# chr4  191154276
+# chr5  180915260
+
+## Output
+# The output file will contain 4 columns defining the depth (vertical coverage)
+# at each position of the selected bed regions. It will be in this format:
+# #Chromosome  Position  Gene_ID  Coverage
+# chr1         73171083  Ew_NG1   7
+# chr1         73171084  Ew_NG1   7
+# chr1         73171085  Ew_NG1   7
+# chr1         73171086  Ew_NG1   8
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                              PARAMETERS                               ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+
+    case $key in
+	-i)
+	  input_file="$2"
+	  shift
+	  ;;
+	-o)
+	  output_prefix="$2"
+	  shift
+	  ;;
+	-b|--bed)
+	  bed_regions="$2"
+	  shift
+	  ;;
+	-g|--gs)
+	  genome_sizes="$2"
+	  shift
+	  ;;
+
+        *)
+            # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
+echo GENOME SIZES    = "${genome_sizes}"
+echo INPUT - BAM/BED = "${input_file}"
+echo OUTPUT PREFIX   = "${output_prefix}"
+echo BED REGIONS     = "${bed_regions}"
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+####                          CREATE BED FROM GTF                          ####
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+## Add option bigwig
+
+
+
+
+# Get per-base coverage
+bedtools coverage -sorted -split -d \
+	-g ${genome_sizes} \
+	-a ${bed_regions} \
+	-b ${input_file} \
+	> ${output_prefix}.cov_per_base
+
+
+
+## Add option bigwig
+
+
+
+## Get final file
+echo -e "#Chromosome\tPosition\tGene_ID\tCoverage" > ${output_prefix}.cov_per_base_final 
+awk -v OFS="\t" '{
+	print $1, $2 + $5 - 1, $4, $6
+   }' ${output_prefix}.cov_per_base \
+   >> ${output_prefix}.cov_per_base_final
+
+
+## Delete intermediary file
+rm -f ${output_prefix}.cov_per_base
+
+
